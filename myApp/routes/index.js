ea7ce8bec25de1a779671e148016f260ac660faf
sinead.71 <cooneysinead71.com>
2016-02-22 21:46:03 +0000

    Adding an if statement to keep the wrong person out of the secrets page. Ran it and it didn't work. Will see if I can fix it now or it will have to wait until next time.

--- myApp/routes/index.js
+++ myApp/routes/index.js
@@ -6,8 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next){
+    if(req.body.yourPassword == "2016" && req.body.youName == "Sinead"){
+        res.render('mySecrets', {
+            yourName:req.body.yourName,
+            yourPassword:req.body.yourPassword
+        });
+    }
+    else {
+        res.redirect('/wrongPerson');
+    }
+});
+
 router.get('/secrets', function(req, res, next){
     res.render('mySecrets', {title: 'Secrets'})
 });
+
 
 module.exports = router;
