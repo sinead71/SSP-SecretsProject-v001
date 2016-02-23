b103116527f1edf686c391fbc0a227263569b9ae
sinead.71 <cooneysinead71.com>
2016-02-23 11:16:05 +0000

    Again tried to fix the problem but no luck so far.

--- myApp/routes/index.js
+++ myApp/routes/index.js
@@ -11,7 +11,7 @@ router.get('/index', function(req, res, next){
 });
 
 router.post('/index', function(req, res, next){
-    if(req.body.yourPassword == "2016" && req.body.youName == "Sinead"){
+    if(req.body.yourPassword == 2016 && req.body.youName == "Sinead"){
         res.render('mySecrets', {
             yourName:req.body.yourName,
             yourPassword:req.body.yourPassword
